0bf9fae3763967b9eebae0152051e0239f22ea01
joseildo filho <joseildofilho@yahoo.com.br>
2020-01-08 10:22:36 -0300

    new code and test

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,56 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+use bit_long_vec::BitLongVec;
 
-pub trait LZW {
-    fn encode(&self);
-    fn new() -> Self;
-    fn dict_ref(&self) -> &HashMap<String, String>;
+pub trait LZW<'lifecycle> {
+    fn encode(msg: &'lifecycle[u8], k: u8) -> Self;
+    fn dict_ref(&self) -> &HashMap<usize, Vec<u8>>;
+    fn msg_ref(&self) -> &'lifecycle [u8];
+    fn codedmsg_ref(&self) -> &BitLongVec;
 }
 
-pub struct LZWDict {
-    dict: HashMap<String, String>
+pub struct LZWData<'lifecycle> {
+    dict    : HashMap<usize, Vec<u8>>,
+    msg     : &'lifecycle [u8],
+    codedmsg: BitLongVec
 }
 
-impl LZW for LZWDict {
-    fn encode(&self) {
-        println!("opá")
+fn create_inital_dict(msg: &[u8]) -> HashMap<usize, Vec<u8>> {
+    if msg.len() == 0 {
+        return HashMap::new();
     }
-    fn new() -> LZWDict {
-        LZWDict {
-            dict: HashMap::new()
+    let mut set:HashSet<u8> = HashSet::new();
+    for c in msg.iter() {
+        set.insert(*c);
+    }
+    let mut vec:Vec<u8> = Vec::with_capacity(set.len());
+    for c in set.drain() {
+        vec.push(c);
+    }
+    vec.sort();
+    let mut map:HashMap<usize, Vec<u8>> = HashMap::new();
+    for (index, c) in vec.drain(0..).enumerate() {
+        map.insert(index, vec![c]);
+    }
+    map
+}
+
+impl<'lifecycle> LZW<'lifecycle> for LZWData<'lifecycle> {
+    fn encode(msg: &'lifecycle[u8], k: u8) -> Self {
+        let dict = create_inital_dict(msg);
+        println!("{:?}", dict);
+        LZWData {
+            dict: dict,
+            msg: msg,
+            codedmsg: BitLongVec::with_fixed_capacity(msg.len(), k)
         }
     }
-    fn dict_ref(&self) -> &HashMap<String, String> {
+    fn dict_ref(&self) -> &HashMap<usize, Vec<u8>> {
         &&self.dict
+    }
+    fn msg_ref(&self) -> &'lifecycle [u8] {
+        &&self.msg
+    }
+    fn codedmsg_ref(&self) -> &BitLongVec {
+        &&self.codedmsg
     }
 }
--- tests/lzw_test.rs
+++ tests/lzw_test.rs
@@ -1,10 +1,28 @@
 #[cfg(test)]
 mod tests {
-    use lzw::{LZW, LZWDict};
+    use lzw::{LZW, LZWData};
     use std::collections::HashMap;
     #[test]
-    fn test_new_lwz() {
-        let compressor:LZWDict = LZW::new();
-        assert_eq!(compressor.dict_ref(), &HashMap::new());
+    fn test_lwz_empty() {
+        let msg: &[u8] = "".as_bytes();
+        let k: u8 = 10;
+        let compressor:LZWData = LZW::encode(msg, k);
+
+        let dict: HashMap<usize, Vec<u8>> = HashMap::new();
+
+        assert_eq!(compressor.dict_ref(), &dict);
+        assert_eq!(compressor.msg_ref(), msg);
+    }
+    #[test]
+    fn test_lwz_one() {
+        let msg: &[u8] = "a".as_bytes();
+        let k: u8 = 10;
+        let compressor:LZWData = LZW::encode(msg, k);
+        
+        let mut dict: HashMap<usize, Vec<u8>> = HashMap::new();
+        dict.insert(0, "a".as_bytes().to_vec());
+
+        assert_eq!(compressor.dict_ref(), &dict);
+        assert_eq!(compressor.msg_ref(), msg);
     }
 }
