1c3e3d409b41474e06e40721973aba667419bfa4
joseildo filho <joseildofilho@yahoo.com.br>
2020-01-26 10:33:48 -0300

    test fix

--- tests/lzw_test.rs
+++ tests/lzw_test.rs
@@ -2,16 +2,18 @@
 mod tests {
     use lzw::{LZW, LZWData};
     use std::collections::HashMap;
+    use bit_long_vec::BitLongVec;
     #[test]
     fn test_lwz_empty() {
         let msg: &[u8] = "".as_bytes();
         let k: u8 = 10;
         let compressor:LZWData = LZW::encode(msg, k);
 
-        let dict: HashMap<usize, Vec<u8>> = HashMap::new();
+        let bits = BitLongVec::with_fixed_capacity(msg.len(), k);
 
-        assert_eq!(compressor.dict_ref(), &dict);
         assert_eq!(compressor.msg_ref(), msg);
+        assert_eq!(compressor.codedmsg_ref(), &bits)
+        
     }
     #[test]
     fn test_lwz_one() {
@@ -19,10 +21,60 @@ mod tests {
         let k: u8 = 10;
         let compressor:LZWData = LZW::encode(msg, k);
         
-        let mut dict: HashMap<usize, Vec<u8>> = HashMap::new();
-        dict.insert(0, "a".as_bytes().to_vec());
+        let mut bits = BitLongVec::with_fixed_capacity(msg.len(), k);
 
-        assert_eq!(compressor.dict_ref(), &dict);
         assert_eq!(compressor.msg_ref(), msg);
+        assert_eq!(compressor.codedmsg_ref(), &bits)
+    }
+    #[test]
+    fn test_lwz_two_new() {
+        let msg: &[u8] = "ab".as_bytes();
+        let k: u8 = 3;
+        let compressor:LZWData = LZW::encode(msg, k);
+        
+        let mut bits = BitLongVec::with_fixed_capacity(msg.len(), k);
+        bits.set(1,1);
+
+        assert_eq!(compressor.msg_ref(), msg);
+        assert_eq!(compressor.codedmsg_ref(), &bits)
+    }
+    #[test]
+    fn test_lwz_three_new() {
+        let msg: &[u8] = "abc".as_bytes();
+        let k: u8 = 3;
+        let compressor:LZWData = LZW::encode(msg, k);
+        
+        let mut bits = BitLongVec::with_fixed_capacity(msg.len(), k);
+        bits.set(1,1);
+        bits.set(2,2);
+
+        assert_eq!(compressor.msg_ref(), msg);
+        assert_eq!(compressor.codedmsg_ref(), &bits)
+    }
+    #[test]
+    fn test_lwz_repeat() {
+        let msg: &[u8] = "abb".as_bytes();
+        let k: u8 = 4;
+        let compressor:LZWData = LZW::encode(msg, k);
+        
+        let mut bits = BitLongVec::with_fixed_capacity(msg.len(), k);
+        bits.set(1,1);
+        bits.set(2,1);
+
+        assert_eq!(compressor.msg_ref(), msg);
+        assert_eq!(compressor.codedmsg_ref(), &bits)
+    }
+    #[test]
+    fn test_lwz_repeat_2() {
+        let msg: &[u8] = "abbb".as_bytes();
+        let k: u8 = 4;
+        let compressor:LZWData = LZW::encode(msg, k);
+        
+        let mut bits = BitLongVec::with_fixed_capacity(msg.len(), k);
+        bits.set(1,1);
+        bits.set(2,3);
+
+        assert_eq!(compressor.msg_ref(), msg);
+        assert_eq!(compressor.codedmsg_ref(), &bits)
     }
 }
